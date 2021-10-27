e9c5fd0c89aba43b95938ffa8cf4929aaf2d2a8a
azattekce <azat.tekce@external.opet.com.tr>
2021-10-27 11:40:57 +0300

    second commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post("/",function(req,res){
 
 });
 
-app.listen(process.env.PORT ||3002,function(){
+app.listen(process.env.PORT ||3000,function(){
     console.log("Server is runing on port 3000");   
 
 });
